e3e3cdae2c3d235563f095a345d3861aa52e2aca
Pavel Senchanka <pavel.senchanka@gmail.com>
2019-04-29 13:47:47 +0200

    Clean up models.

--- packages/contracts/scripts/test.sh
+++ packages/contracts/scripts/test.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+set -e
+
 npx ganache-cli -a 100 -i 3  >/dev/null &
 GANACHE_PID=$!
+trap "kill $GANACHE_PID" EXIT
+
 echo "Started Ganache, PID: $GANACHE_PID"
 
 npx truffle test "$@"
 
 echo "Tests ran"
-kill $GANACHE_PID
