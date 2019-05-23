075b19664c0c026008a3edbd1048932bd44e2440
Pavel Senchanka <pavel.senchanka@gmail.com>
2019-05-24 00:14:59 +0200

    Clean up.

--- packages/contracts/scripts/test.sh
+++ packages/contracts/scripts/test.sh
@@ -3,12 +3,12 @@
 set -e
 set -x
 
-yarn ganache-cli -a 100 -i 3  >/dev/null &
+npx ganache-cli -a 100 -i 3  >/dev/null &
 GANACHE_PID=$!
 trap "kill $GANACHE_PID" EXIT
 
 echo "Started Ganache, PID: $GANACHE_PID"
 
-yarn truffle test "$@"
+npx truffle test "$@"
 
 echo "Tests ran"
