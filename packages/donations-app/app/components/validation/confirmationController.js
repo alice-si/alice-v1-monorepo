b9f0b9dcb446a190c0a93d57782e9b9b01bbb338
alexAlice <alex@alice.si>
2019-06-19 11:26:21 +0200

    added loading spinner to validation dashboard

--- packages/donations-app/app/components/validation/confirmationController.js
+++ packages/donations-app/app/components/validation/confirmationController.js
@@ -13,13 +13,16 @@ angular.module('aliceApp')
             validationId: validation._id,
             password: vm.password,
           };
+          vm.showSpinner = true;
           $http.post(API + 'approveClaim', payload).then(response => {
+            vm.showSpinner = false;
             vm.confirmationError = null;
             $scope.$dismiss();
             NotificationService.success(
               'Claim has been successfully approved.');
             $rootScope.$broadcast('validation');
           }).catch(response => {
+            vm.showSpinner = false;
             vm.confirmationError = response.data;
           });
         };
