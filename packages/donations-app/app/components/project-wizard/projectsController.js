843f9d207efcf1288c437028355076fcc989310e
alexAlice <42033996+alexAlice@users.noreply.github.com>
2019-07-23 15:41:27 +0200

    project deployment with button implemented (#41)
    * project deployment with button implemented
    
    * Added button to make projects active

--- packages/donations-app/app/components/project-wizard/projectsController.js
+++ packages/donations-app/app/components/project-wizard/projectsController.js
@@ -1,9 +1,32 @@
 angular.module('aliceApp')
 
-  .controller('ProjectsController', ['AuthService', '$http', 'API', function (AuthService, $http, API) {
+  .controller('ProjectsController', ['AuthService', 'NotificationService', '$http', '$state', 'API', 'ETHERSCAN', function (AuthService, NotificationService, $http, $state, API, ETHERSCAN) {
     var vm = this;
     vm.auth = AuthService;
-  
+
+    vm.ETHERSCAN = ETHERSCAN;
+
+    vm.startDeployment = function ({ code }) {
+      const question =
+        `Are you sure you want to deploy the project "${code}" to blockchain?`
+        + ` Project status will be changed to "CREATED" and`
+        + ` background blockchain jobs will start project deployment.`;
+      const successMsg = `Project deployment started for project: ${code}`;
+      setProjectStatus(code, 'CREATED', question, successMsg);
+    };
+
+    vm.makeActive = function ({ code }) {
+      const question =
+        `Are you sure you want to make the project "${code}" active?`
+        + ` Users will be able to make donations to it.`
+      const successMsg = `Project is active now: ${code}`;
+      setProjectStatus(code, 'ACTIVE', question, successMsg);
+    };
+
+    vm.noAddresseForProject = function ({ code }) {
+      NotificationService.error(`Project "${code}" does not have an ethAddress`);
+    }
+
     if (!AuthService.getLoggedUser()) {
       AuthService.showLogInModal();
       loadData();
@@ -15,6 +38,18 @@ angular.module('aliceApp')
       $http.get(API + 'getProjectsForAdmin').then(function (projects) {
         vm.projects = projects.data;
       });
+    }
+
+    function setProjectStatus(code, status, question, successMsg) {
+      if (confirm(question)) {
+        $http.post(API + 'setProjectStatus', {
+          code,
+          status,
+        }).then(function () {
+          NotificationService.success(successMsg);
+          $state.reload();
+        });
+      }
     }
 
     return vm;
