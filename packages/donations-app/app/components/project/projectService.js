2c129d3d1af40c3bb23a1232a4d3cb605120f694
alexAlice <alex@alice.si>
2019-08-06 17:17:39 +0200

    minor fix

--- packages/donations-app/app/components/project/projectService.js
+++ packages/donations-app/app/components/project/projectService.js
@@ -70,17 +70,6 @@ angular.module('aliceApp')
       return details;
     };
 
-    this.getAppealPageVersion = function (projectCode) {
-      const appealVersionsForProject = {
-        'fusion-housing-1': 2,
-        // 'gift-of-walking': 2,
-        // 'save-from-abuse': 2
-      };
-      const defaultAppealVersion = 4;
-
-      return appealVersionsForProject[projectCode] || defaultAppealVersion;
-    }
-
     this.model = model;
 
   }]);
