48fe4c7c4c8f129c237f954e0930b07069a48953
Dhen E Padilla <dhenepadilla@gmail.com>
2019-09-13 18:28:46 +0100

    splash card consolidation

--- packages/donations-app/app/components/my-impact/myImpactController.js
+++ packages/donations-app/app/components/my-impact/myImpactController.js
@@ -91,6 +91,11 @@ angular.module('aliceApp')
               });
             }
 
+						// TODO - Dhen: Fix this in backend 
+						// For splash consolidation
+						vm.project.raised = vm.project.donatedByAll;
+						vm.project._outcomes = vm.project.outcomes;
+
 						console.log(vm.project);
           }
 				}
