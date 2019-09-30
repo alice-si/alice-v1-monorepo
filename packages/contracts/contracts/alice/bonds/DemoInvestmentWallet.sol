ca1787d120476f9fa6c2ec8144e774990c657e78
Dhen E Padilla <dhenepadilla@gmail.com>
2019-09-30 19:26:43 +0100

    reverted change

--- packages/contracts/contracts/alice/bonds/DemoInvestmentWallet.sol
+++ packages/contracts/contracts/alice/bonds/DemoInvestmentWallet.sol
@@ -14,12 +14,4 @@ contract DemoInvestmentWallet is InvestmentWallet {
         _token.mint(_amount);
     }
 
-    function investAndRedeem(uint _amount, string memory _projectName) public onlyOwner {
-        invest(_amount, _projectName);
-
-        address projectAddress = projectCatalog.getProjectAddress(_projectName);
-        ERC20 couponToken = ProjectWithBonds(projectAddress).getCoupon();
-        couponToken.transfer(msg.sender, couponToken.balanceOf(address(this)));
-    }
-
 }
