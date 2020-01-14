6d6b09cefc742e36626817b101cbd58de0120331
hatskier <Suvorovalex1997@gmail.com>
2020-01-14 13:01:34 +0100

    scrolling to projects implemented, agreeContact mechanism updates

--- packages/donations-app/app/components/auth/registrationController.js
+++ packages/donations-app/app/components/auth/registrationController.js
@@ -5,7 +5,7 @@ angular.module('aliceApp')
     vm.credentials = {};
     vm.registration = {
       agreeAlice: true,
-      agreeContact: true,
+      agreeContact: false,
     };
     vm.mode = 'REGISTRATION'; // enum: ['REGISTRATION', 'REGISTRATION_FININSHING']
     if (AuthService.getEmailForSignupFinishing()) {
@@ -56,9 +56,7 @@ angular.module('aliceApp')
     };
 
     let extendContactAgreement = function() {
-      let flag = vm.registration.disagreeContact;
-      vm.registration.agreeContact = !flag;
-      vm.registration.agreeAlice = !flag;
+      vm.registration.agreeAlice = vm.registration.agreeContact;
     }
 
     return vm;
